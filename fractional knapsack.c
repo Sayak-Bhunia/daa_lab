42ce7113476662e047703eb80b5ac111df69fd63
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-05-19 23:03:32 +0530

    Update fractional knapsack.c

--- fractional knapsack.c
+++ fractional knapsack.c
@@ -1,5 +1,6 @@
+// Online C compiler to run C program online
 #include <stdio.h>
-#include <stdlib.h>
+#include<stdlib.h>
 
 struct item {
     int val;
@@ -7,34 +8,34 @@ struct item {
 };
 
 int helper(const void *a, const void *b) {
-    double r1 = (double)((struct item *)a)->val/(double)((struct item *)a)->wt;
-    double r2 = (double)((struct item *)b)->val/(double)((struct item *)b)->wt;
-    if(r1 > r2) return -1;
-    else if(r1 < r2) return 1; 
+    double r1 = ((struct item *)a)->val/(double)((struct item *)a)->wt;
+    double r2 = ((struct item *)b)->val/(double)((struct item *)b)->wt;
+    if(r1>r2) return -1;
+    else if(r1<r2) return 1;
     else return 0;
 }
 
-double fractionalKnapsack(int w, struct item a[], int n) {
-    qsort(a, n, sizeof(a[0]), helper); 
-    int curr = 0;
-    double final = 0.0;
+double fractionalKnapsack(struct item a[], int n, int w) {
+    qsort(a, n, sizeof(a[0]), helper);
+    int c = 0;
+    double f = 0.0;
     for(int i=0;i<n;i++) {
-        if(curr + a[i].wt <= w) {
-            curr += a[i].wt;
-            final += a[i].val;
-        } 
+        if(c + a[i].wt <= w) {
+            c += a[i].wt;
+            f += a[i].val;
+        }
         else {
-            int rem = w - curr;
-            final += ((double)a[i].val/(double)a[i].wt)*(double)rem;
+            int r = w - c;
+            f += (a[i].val/(double)a[i].wt)*r;
             break;
         }
     }
-    return final;
+    return f;
 }
 
 int main() {
     int n = 3, w = 50;
     struct item a[] = {{100, 20}, {60, 10}, {120, 30}};
-    double res = fractionalKnapsack(w, a, n);
-    printf("The maximum value is %.2lf\n", res);
+    double res = fractionalKnapsack(a, n, w);
+    printf("max val = %.2lf\n",res);
 }
