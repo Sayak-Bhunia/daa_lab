54202fc2ee0e1fa3291a986eec8ced020123b0eb
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-05-23 23:03:33 +0530

    Update heapSort.c

--- heapSort.c
+++ heapSort.c
@@ -1,4 +1,5 @@
-#include<stdio.h>
+// Online C compiler to run C program online
+#include <stdio.h>
 
 void swap(int *a, int *b) {
     int z = *a;
@@ -6,40 +7,31 @@ void swap(int *a, int *b) {
     *b = z;
 }
 
-int heapify(int a[], int n, int i) {
-    int largest = i;
+void heapify(int a[], int n, int i) {
+    int mx = i;
     int l = 2*i+1;
     int r = 2*i+2;
-    if(l<n && a[l]>a[largest]) largest = l;
-    if(r<n && a[r]>a[largest]) largest = r;
-    if(largest != i) {
-        swap(&a[i], &a[largest]);
-        heapify(a, n, largest);
+    if(l<n && a[l]>a[mx]) mx = l;
+    if(r<n && a[r]>a[mx]) mx = r;
+    if(mx != i) {
+        swap(&a[i], &a[mx]);
+        heapify(a, n, mx);
     }
 }
 
 void heapSort(int a[], int n) {
-    for(int i=n/2-1;i>=0;i--) {
-        heapify(a, n, i);
-    }
+    for(int i=n/2-1;i>=0;i--) heapify(a, n, i);
     for(int i=n-1;i>=0;i--) {
         swap(&a[0], &a[i]);
         heapify(a, i, 0);
     }
 }
 
-int main()
-{
-    int a[8];
-    printf("enter elements: ");
-    for(int i=0;i<8;i++) {
-        scanf("%d",&a[i]);
-    }
-    heapSort(a,8);
-    printf("\nSorted Array:\n");
-    for(int i=0;i<8;i++) {
-        printf("%d ",a[i]);
-    }
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    heapSort(a, n);
+    for(int i=0;i<n;i++) printf("%d ",a[i]);
 }
-
-// 7 19 32 16 5 13 67 90
