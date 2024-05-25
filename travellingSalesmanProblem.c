14ec31411f1f6ca11db566a5735fc82ab3b95395
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-05-25 22:54:11 +0530

    Update travellingSalesmanProblem.c

--- travellingSalesmanProblem.c
+++ travellingSalesmanProblem.c
@@ -1,51 +1,45 @@
+// Online C compiler to run C program online
 #include <stdio.h>
-int tsp_g[10][10] = {
+#define inf 9999
+#define m 10
+int a[m][m] = {
    {12, 30, 33, 10, 45},
    {56, 22, 9, 15, 18},
    {29, 13, 8, 5, 12},
    {33, 28, 16, 10, 3},
    {1, 4, 30, 24, 20}
 };
-int vis[10], n, cost = 0;
+int vis[m], n, cost = 0;
 
-/* creating a function to generate the shortest path */
-void travellingsalesman(int c){
-   int k, adjv = 999;
+void tsp(int c) {
+   int adjv = 999;
    int mn = 999;
-   
-   /* marking the vertices vis in an assigned array */
    vis[c] = 1;
-   
-   /* displaying the shortest path */
-   printf("%d ", c + 1);
-   
-   /* checking the minimum cost edge in the graph */
-   for(k = 0; k < n; k++) {
-      if((tsp_g[c][k] != 0) && (vis[k] == 0)) {
-         if(tsp_g[c][k]<mn) mn = tsp_g[c][k];
-         adjv = k;
+   printf("%d ", c+1);
+   for(int i=0;i<n;i++) {
+      if(a[c][i] != 0 && vis[i] == 0) {
+         if(a[c][i]<mn) mn = a[c][i];
+         adjv = i;
       }
    }
    if(mn != 999) cost += mn;
    if(adjv == 999) {
       adjv = 0;
-      printf("%d", adjv + 1);
-      cost += tsp_g[c][adjv];
+      printf("%d", adjv+1);
+      cost += a[c][adjv];
       return;
    }
-   travellingsalesman(adjv);
+   tsp(adjv);
 }
 
-/* main function */
-int main(){
-   int i, j;
-   n = 5;
-   for(i = 0; i < n; i++) vis[i] = 0;
-   printf("Shortest Path: ");
-   travellingsalesman(0);
-   printf("\nMinimum Cost: ");
-   printf("%d\n", cost);
-   return 0;
+int main() {
+    int i,j;
+    n = 5;
+    for(i=0;i<n;i++) vis[i] = 0;
+    printf("shortest path = ");
+    tsp(0);
+    printf("\nmin cost = ");
+    printf("%d\n",cost);
+    
+    
 }
-
-
