f3a29181de95f922e8723e917ba5a4541f96a93b
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-05-25 21:44:46 +0530

    Update Dijkstra's algo.c

--- Dijkstra's algo.c
+++ Dijkstra's algo.c
@@ -1,72 +1,60 @@
+// Online C compiler to run C program online
 #include <stdio.h>
-#define inf 9999 //INFINITY
-#define mx 10
+#define inf 9999
+#define m 10
 
-void Dijkstra(int Graph[mx][mx], int n, int start) {
-  int cost[mx][mx], dis[mx], pred[mx];
-  int visited[mx], c, mndis, next, i, j;
-
-  // Creating cost matrix
-  for (i = 0; i < n; i++)
-    for (j = 0; j < n; j++)
-      if (Graph[i][j] == 0)
-        cost[i][j] = inf;
-      else
-        cost[i][j] = Graph[i][j];
-
-  for (i = 0; i < n; i++) {
-    dis[i] = cost[start][i];
-    pred[i] = start;
-    visited[i] = 0;
-  }
-
-  dis[start] = 0;
-  visited[start] = 1;
-  c = 1;
-
-  while (c < n - 1) {
-    mndis = inf;
-
-    for (i = 0; i < n; i++)
-      if (dis[i] < mndis && !visited[i]) {
-        mndis = dis[i];
-        next = i;
-      }
-
-    visited[next] = 1;
-    for (i = 0; i < n; i++)
-      if (!visited[i])
-        if (mndis + cost[next][i] < dis[i]) {
-          dis[i] = mndis + cost[next][i];
-          pred[i] = next;
+void dijkstra(int a[m][m], int n, int st) {
+    int cost[m][m], dis[m], pred[m];
+    int vis[m], mndis, c, next;
+    
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            if(a[i][j] == 0) cost[i][j] = inf;
+            else cost[i][j] = a[i][j];
         }
-    c++;
-  }
-
-  // Printing the dis
-  for (i = 0; i < n; i++)
-    if (i != start) {
-      printf("\ndis from source to %d: %d", i, dis[i]);
+    }
+    
+    for(int i=0;i<n;i++) {
+        dis[i] = cost[st][i];
+        pred[i] = st;
+        vis[i] = 0;
+    }
+    
+    dis[st] = 0;
+    vis[st] = 1;
+    c = 1;
+    
+    while(c<n-1) {
+        mndis = inf;
+        for(int i=0;i<n;i++) {
+            if(dis[i]<mndis && !vis[i]) {
+                mndis = dis[i];
+                next = i;
+            }
+        }
+        vis[next] = 1;
+        for(int i=0;i<n;i++) {
+            if(!vis[i]) {
+                if(mndis+cost[next][i]<dis[i]) {
+                    dis[i] = mndis+cost[next][i];
+                    pred[i] = next;
+                }
+            }
+        }
+        c++;
+    }
+    for(int i=0;i<n;i++) {
+        if(i != st) printf("\n dis from source to %d = %d", i, dis[i]);
     }
 }
 
 int main() {
-  int Graph[mx][mx], i, j, n, u;
-  
-  printf("Enter the number of nodes: ");
-  scanf("%d", &n);
-
-  printf("Enter the adjacency matrix for the graph:\n");
-  for (i = 0; i < n; i++) {
-    for (j = 0; j < n; j++) {
-      scanf("%d", &Graph[i][j]);
+    int a[m][m];
+    int n,u;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) scanf("%d",&a[i][j]);
     }
-  }
-
-  printf("Enter the starting node: ");
-  scanf("%d", &u);
-
-  Dijkstra(Graph, n, u);
-
-  return 0;
+    scanf("%d",&u);
+    dijkstra(a, n, u);
 }
