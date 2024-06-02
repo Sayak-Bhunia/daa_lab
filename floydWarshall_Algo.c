35ab5429879a96bef5c9fe419a0900af4d8dcd18
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-06-02 13:20:52 +0530

    Update floydWarshall_Algo.c

--- floydWarshall_Algo.c
+++ floydWarshall_Algo.c
@@ -1,8 +1,24 @@
+// Online C compiler to run C program online
 #include<stdio.h>
 #define n 4
 #define inf 999
 
-void printMatrix(int a[][n]) {
+void floydWarshall(int a[][n]) {
+    for(int k=0;k<n;k++) {
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++) {
+                if(a[i][k]+a[k][j]<a[i][j]) a[i][j] = a[i][k] + a[k][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int a[n][n] = {{0, 3, inf, 5},
+                   {2, 0, inf, 4},
+                   {inf, 1, 0, inf},
+                   {inf, inf, 2, 0}};
+    floydWarshall(a);
     for(int i=0;i<n;i++) {
         for(int j=0;j<n;j++) {
             if(a[i][j] == inf) printf("0");
@@ -10,26 +26,4 @@ void printMatrix(int a[][n]) {
         }
         printf("\n");
     }
-}
-
-void floydWarshall(int a[][n]) {
-    for(int k=0;k<n;k++) {
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                if(a[i][k] + a[k][j]<a[i][j]) {
-                    a[i][j] = a[i][k] + a[k][j];
-                }
-            }
-        }
-    }
-    printMatrix(a);
-}
-
-int main()
-{
-    int a[n][n] = {{0, 3, inf, 5},
-                   {2, 0, inf, 4},
-                   {inf, 1, 0, inf},
-                   {inf, inf, 2, 0}};
-    floydWarshall(a);
 }
