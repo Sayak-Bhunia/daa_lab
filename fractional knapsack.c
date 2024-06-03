9702a14a8a4d993799ed48fa869aa23192af6d90
Sayak Bhunia <110457746+Sayak-Bhunia@users.noreply.github.com>
2024-06-03 14:10:29 +0530

    Update fractional knapsack.c

--- fractional knapsack.c
+++ fractional knapsack.c
@@ -1,32 +1,34 @@
-// Online C compiler to run C program online
-#include <stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<stdbool.h>
 
-struct item {
+typedef struct item {
     int val;
     int wt;
-};
+}item;
 
 int helper(const void *a, const void *b) {
-    double r1 = ((struct item *)a)->val/(double)((struct item *)a)->wt;
-    double r2 = ((struct item *)b)->val/(double)((struct item *)b)->wt;
+    int r1 = ((item *)a)->val/(double)((item *)a)->wt;
+    int r2 = ((item *)b)->val/(double)((item *)b)->wt;
     if(r1>r2) return -1;
     else if(r1<r2) return 1;
     else return 0;
 }
 
-double fractionalKnapsack(struct item a[], int n, int w) {
+double fractionalKnapsack(item a[], int n, int w) {
     qsort(a, n, sizeof(a[0]), helper);
     int c = 0;
     double f = 0.0;
     for(int i=0;i<n;i++) {
-        if(c + a[i].wt <= w) {
+        if(c+a[i].wt<=w) {
             c += a[i].wt;
             f += a[i].val;
         }
         else {
             int r = w - c;
-            f += (a[i].val/(double)a[i].wt)*r;
+            f += ((a[i].val)/(double)(a[i].wt))*r;
             break;
         }
     }
@@ -35,7 +37,6 @@ double fractionalKnapsack(struct item a[], int n, int w) {
 
 int main() {
     int n = 3, w = 50;
-    struct item a[] = {{100, 20}, {60, 10}, {120, 30}};
-    double res = fractionalKnapsack(a, n, w);
-    printf("max val = %.2lf\n",res);
+    item a[] = {{100, 20}, {60, 10}, {120, 30}};
+    printf("%.2lf",fractionalKnapsack(a, n, w));
 }
